f7c4a5026dca89ef19c5f77d9c01bc26d53c15e7
n1c00o <git.n1c00o@gmail.com>
2020-11-25 21:48:42 +0100

    Add base of the app
    todo: redirections, find a link...

---
+++ index.js
@@ -0,0 +1,4 @@
+const apiIssuesURL = "https://api.github.com/repos/n1c00o/xn--7biy268m.ml/issues";
+const linkLabel = "link"; // name of the label issues containing a link have
+
+function main() {}
