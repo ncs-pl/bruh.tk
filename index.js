208d9898ac5d52eb7c60f0ba06ebb969cd72c553
n1c00o <git.n1c00o@gmail.com>
2020-11-26 19:27:01 +0100

    Add Issue detection

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-const apiIssuesURL = "https://api.github.com/repos/n1c00o/xn--7biy268m.ml/issues";
-const linkLabel = "link"; // name of the label issues containing a link have
+const apiURL = "https://api.github.com/repos/n1c00o/xn--7biy268m.ml/issues"; // Issues endpoint of your repo
+const labelID = 2531752619; // ID of the label for links
 
-function main() {}
+function redirectToHome() {
+	window.location.replace(window.location.hostname);
+}
+
+async function main() {
+	// Get the shortened tag (which correspond to an issue number)
+	const tag = window.location.pathname.replace("/", "").split("/")[0];
+	const res = await fetch(
+		`${apiURL}/${tag}`,
+		{
+			method: "GET",
+			headers: {
+				accept: "application/json"
+			},
+			body: {}
+		});
+
+		if (res.status !== 200) return redirectToHome();
+
+		const body = await res.json();
+
+		// Return to home if the issue is not labelled with the wanted label or if the title is not an URL
+		if (
+			body.labels.filter(
+				(label) => label.id === labelID
+			).length < 0 ||
+			!new RegExp(/(https?:\/\/)?([\w\-])+\.{1}([a-zA-Z]{2,63})([\/\w-]*)*\/?\??([^#\n\r]*)?#?([^\n\r]*)/).test(body.title)
+		) return redirectToHome();
+
+		window.location.replace(body.title);
+}
+
+main();
